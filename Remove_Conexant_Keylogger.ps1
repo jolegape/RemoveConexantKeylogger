b4ee76679fd1d4223f87b1135c51f7671ede6ac6
Gavin Willett <jolegape@users.noreply.github.com>
2017-05-11 23:16:37 +1000

    Script to remove MicTray and log file

---
+++ Remove_Conexant_Keylogger.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.SYNOPSIS
+    Removes Conexant MicTray and associated log file
+.DESCRIPTION
+    I first came across this on a Reddit 
+    post (https://www.reddit.com/r/sysadmin/comments/6ajfpc/keylogger_in_hp_conexant_hd_audio_audio_driver/)
+    Following the proof of concept in the linked blog post I was able to replicate this exploit.
+    This script stops the running process and removes the offending files. Needs to be run as admin.
+.NOTES
+    FileName:       Remove_Conexant_Keylogger.ps1
+    Author:         Gavin Willett
+.LINK
+    Script hosted at:
+    http://github.com/jolegape
+#>
+
+if (Test-path "$($env:SystemRoot)\System32\Mictray64.exe"){
+    Stop-Process -Name MicTray64 -Force
+    Remove-Item -Path "$($env:SystemRoot)\System32\Mictray64.exe" -Force
+} elseif (Test-Path "$($env:SystemRoot)\System32\Mictray.exe"){ 
+    Stop-Process -Name MicTray -Force
+    Remove-Item -Path "$($env:SystemRoot)\System32\Mictray.exe" -Force
+}
+
+if (Test-path "$($env:SystemDrive)\Users\Public\MicTray.log"){
+    Remove-Item -Path "$($env:SystemDrive)\Users\Public\MicTray.log" -Force
+}
